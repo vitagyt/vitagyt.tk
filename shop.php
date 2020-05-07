e0563bdc903b40dee90caa2af0e6ba537601050c
Vitag <vitag.creator@gmail.com>
2020-05-07 11:37:58 +0200

    version beta, ajout de fonctionnalité

---
+++ shop.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include('html/meta.html'); ?>
+    <title>vitagyt.tk - Shop</title>
+</head>
+<body>
+    <?php include('html/header.html'); ?>
+
+    <div id="shop_div">
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeO6zcV5a-2lutM7syCg3U6h9P-xVk5fcaWJ8c3je_kYrDl0g/viewform?embedded=true" width="640" height="1990" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>
+    </div>
+
+    <?php include('html/footer.html'); ?>
+</body>
+</html>
