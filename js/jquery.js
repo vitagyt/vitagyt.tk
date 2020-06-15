495fe277c16b60b1c0d55957b7b54e3531225d63
Vitag <vitag.creator@gmail.com>
2020-06-15 11:18:18 +0200

    include en javascript

---
+++ js/jquery.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){$("#header").load('html/header.html');});
+
+$(document).ready(function(){$("#footer").load('html/footer.html');});
+
+$(document).ready(function(){$("#seo").load('html/seo.html');});
