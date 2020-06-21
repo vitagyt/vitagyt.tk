7d5154713a75746628c2bec4738c78a2c4246a40
Vitag <vitag.creator@gmail.com>
2020-06-21 20:51:09 +0200

    Correction code

--- js/jquery.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){$("#header").load('html/header.html');});
-
-$(document).ready(function(){$("#footer").load('html/footer.html');});
-
-$(document).ready(function(){$("#seo").load('html/seo.html');});
